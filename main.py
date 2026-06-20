0aaae1b44873c302562af6eda41f405baf734fd3
Murari Charan <muraricharan2@gmail.com>
2026-06-20 14:34:14 +0530

    Enhanced portfolio UI with project details and certificate timeline

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ import os
 app = FastAPI()
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=465,
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME", "muraricharan2@gmail.com"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD", "mznktgiqsbftgexq"),
+    MAIL_FROM=os.getenv("MAIL_FROM", "muraricharan2@gmail.com"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True,
 )
 
